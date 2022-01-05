d29b962a6b028c96042ea3ad2615c5b2b5c05840
Anita Tabaczka <atabaczka@edu.cdv.pl>
2022-01-06 00:57:58 +0100

    jwt auth API rollback

--- restaurantManager/config/app.php
+++ restaurantManager/config/app.php
@@ -139,7 +139,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -190,8 +189,7 @@ return [
     */
 
     'aliases' => [
-        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
