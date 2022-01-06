83d049fe54bd6bd9f64a0f12ee127e5c8a6a1919
Anita Tabaczka <atabaczka@edu.cdv.pl>
2022-01-06 17:41:24 +0100

    JWT auth

--- restaurantManager/app/Http/Controllers/AuthController.php
+++ restaurantManager/app/Http/Controllers/AuthController.php
@@ -1,61 +1,55 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+    }
+
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-        try{
-        if (Auth::attempt($credentials)) {
-            /** @var User $user */
-            $user = Auth::user();
-            $token = $user->createToken('app')->accessToken;
-
-            return response()->json([
-                'success' => true,
-                'token' => $token,
-                'user' => $user
-            ], 200);
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'message' => 'Invalid credentials!'
+            ], Response::HTTP_UNAUTHORIZED);
         }
-    }catch (\Exception $exception){
-        return response([
-            'message' => $exception->getMessage()
-        ], 400);
-    }
-        return response()->json(
+        /** @var \App\Models\MyUserModel $user **/
+        $user = Auth::user();
 
-            [
-            'success' => false, 
-            'error' => ["message" => 'Invalid email address and / or password.'] 
-            ], 403
-        );
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 24); // 1 day
+
+        return response([
+            'message' => $token
+        ])->withCookie($cookie);
     }
-    public function user(){
+
+    public function user()
+    {
         return Auth::user();
     }
-    
-    public function register(Request $request){
-       $validator = Validator::make($request->all(),[
-           'first_name' => ['required', 'string', 'max:255'],
-           'last_name' => ['required', 'string', 'max:255'],
-           'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-           'password' => ['required', 'string', 'min:8'],
-       ]);
-       if($validator->fails()){
-           return response()->json(['error' => $validator->errors()], 422);
-       }
-       $input = $request->all();
-       $input['password'] = Hash::make($input['password']);
-       $user = User::create($input);
-       Auth::login($user);
-       return response()->json(['success' => true], 200);
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+
+        return response([
+            'message' => 'Success'
+        ])->withCookie($cookie);
     }
 }
--- restaurantManager/routes/api.php
+++ restaurantManager/routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
     
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::apiResource('ingredients', ingredientsController::class);
 
@@ -35,11 +36,14 @@ Route::delete('mealsIngredients/{MealIngId}/delete', [MealsIngredientsController
 Route::get('menu', [MenuController::class, 'index']);
 Route::post('menu/add', [MenuController::class, 'addToMenu']);
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register',[AuthController::class, 'register']);
-Route::post('forgot', [ForgotController::class, 'forgot']);
-Route::post('reset', [ForgotController::class, 'reset']);
-Route::get('user', [AuthController::class, 'user'])->middleware('auth:api');
 Route::post('add/meal', [MealsController::class, 'store']);
 Route::get('delete/meal/{MealId}', [MealsController::class, 'destroy']);
 Route::post('update/meal/{MealId}', [MealsController::class, 'update']);
+
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+});
